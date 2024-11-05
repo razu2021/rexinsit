18d02f5f441826271a49f01750e6a6dae0e29454
Md Razu Hossain Raj <mdrazuhossainraj@gmail.com>
2024-11-05 17:39:12 +0600

    new update
    new update

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
-$uri = urldecode(
-    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
-);
-if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
-    return false;
+
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'//storage/framework/maintenance.php')) {
+    require $maintenance;
 }
-require_once __DIR__.'/public/index.php';
+
+// Register the Composer autoloader...
+require __DIR__.'/RexInsIt/vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__.'/RexInsIt/bootstrap/app.php')
+    ->handleRequest(Request::capture());
