d0d21c4b097d149b503b2a8d3498aa16833db7ae
Md Razu Hossain Raj <mdrazuhossainraj@gmail.com>
2024-11-06 12:49:58 +0600

    update
    update

--- index.php
+++ index.php
@@ -10,8 +10,8 @@ if (file_exists($maintenance = __DIR__.'//storage/framework/maintenance.php')) {
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/RexInsIt/vendor/autoload.php';
+require __DIR__.'/rexinsit/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/RexInsIt/bootstrap/app.php')
+(require_once __DIR__.'/rexinsit/bootstrap/app.php')
     ->handleRequest(Request::capture());
