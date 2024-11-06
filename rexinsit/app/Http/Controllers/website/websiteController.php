ade89e1d45b32268c24f3aca3f25513f5b25892c
Md Razu Hossain Raj <mdrazuhossainraj@gmail.com>
2024-11-06 13:10:31 +0600

    update
    update

---
+++ rexinsit/app/Http/Controllers/website/websiteController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\website;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class websiteController extends Controller
+{
+    
+
+public function index(){
+    return view('website.index');
+}
+
+}
--- rexinsit/routes/web.php
+++ rexinsit/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\website\websiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('index',[websiteController::class,'index']);
