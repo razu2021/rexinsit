0bad83b8a39a5183870e47df39c02aa8cb8049f4
Md Razu Hossain Raj <mdrazuhossainraj@gmail.com>
2024-11-06 14:11:31 +0600

    update
    update

--- rexinsit/resources/views/website/index.blade.php
+++ rexinsit/resources/views/website/index.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.Webmaster')
 @section('web_content')
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Rex-Innovative & Solutions</title>
 </head>
 <body>
     <h1>this is rexins it </h1>
